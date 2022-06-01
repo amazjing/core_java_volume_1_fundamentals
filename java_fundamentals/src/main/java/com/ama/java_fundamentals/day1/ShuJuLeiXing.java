3b52b735330b0c7df3ec99e59b2c3d7f339b90e3
amazjing <amazjing@163.com>
2022-06-01 11:23:04 +0800

    结合赋值与运算符

--- java_fundamentals/src/main/java/com/ama/java_fundamentals/day1/ShuJuLeiXing.java
+++ java_fundamentals/src/main/java/com/ama/java_fundamentals/day1/ShuJuLeiXing.java
@@ -97,5 +97,114 @@ public class ShuJuLeiXing {
          */
     }
 
+    /**
+     * 数学函数与常量
+     */
+    @Test
+    public void test4() {
+        double x = 4;
+        double y = Math.sqrt(x);
+        System.out.println("y = " + y);
+        /**
+         * 输出：y = 2.0
+         */
+    }
+
+    /**
+     * 幂运算
+     */
+    @Test
+    public void test5() {
+        double y = Math.pow(2.0, 3.0);
+        System.out.println("y = " + y);
+        /**
+         * 输出：y = 8.0
+         */
+    }
+
+    /**
+     * 数值类型之间的转换
+     */
+    @Test
+    public void test6() {
+        int n = 123456789;
+        float b = n;
+        System.out.println("b = " + b);
+        /**
+         * 输出：b = 1.23456792E8
+         */
+    }
+
+    /**
+     * 强制类型转换
+     * 结果：整数
+     */
+    @Test
+    public void test7() {
+        double x = 9.997;
+        int nx = (int) x;
+        System.out.println("nx = " + nx);
+        /**
+         * 输出：nx = 9
+         */
+    }
+
+    /**
+     * 强制类型转换；舍入运算
+     */
+    @Test
+    public void test8() {
+        double x = 9.997;
+        int nx = (int) Math.round(x);
+        System.out.println("nx = " + nx);
+        /**
+         * 输出：nx = 10
+         */
+    }
+
+    /**
+     * 强制类型转换；超出强制转换类型的范围
+     */
+    @Test
+    public void test9() {
+        int a = 300;
+        byte b = (byte) a;
+        System.out.println("b = " + b);
+        /**
+         * 输出：b = 44
+         */
+    }
+
+    /**
+     * 结合赋值和运算符
+     */
+    @Test
+    public void test10() {
+        int x = 2;
+        x += 4;
+        System.out.println("x = " + x);
+        /**
+         * 输出：x = 6
+         */
+    }
+
+    /**
+     * 自增和自减运算符
+     */
+    @Test
+    public void test11() {
+        int x = 2;
+        x++;
+        System.out.println("x = " + x);
+        /**
+         * 输出：x = 3
+         */
+        int y = 2;
+        y--;
+        System.out.println("y = " + y);
+        /**
+         * 输出：y = 1
+         */
+    }
 
 }
